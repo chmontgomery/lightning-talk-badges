bdd435e9b4a908d5b7c404b545532ad98b6eee52
Chris Montgomery <chr.montgomery@gmail.com>
2015-04-13 14:14:10 -0500

    log errors and send 500 if server throws error

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var viewModel = require('../lib/view-model');
 var spreadsheetService = require('../lib/services/spreadsheet-service');
+var log = require('../lib/services/logger-service');
 
 module.exports = function (app) {
   app.use('/', router);
@@ -12,15 +13,24 @@ router.get('/', function (req, res) {
 });
 
 router.get('/data', function (req, res) {
-  viewModel().then(function (data) {
-    res.json(data);
-  });
+  viewModel()
+    .then(function (data) {
+      res.json(data);
+    })
+    .catch(function (err) {
+      log.error(err);
+      res.send(500);
+    });
 });
 
 router.get('/people', function (req, res) {
   spreadsheetService("People")
     .then(function (data) {
       res.json(data);
+    })
+    .catch(function (err) {
+      log.error(err);
+      res.send(500);
     });
 });
 
@@ -28,6 +38,10 @@ router.get('/badges', function (req, res) {
   spreadsheetService("Badges")
     .then(function (data) {
       res.json(data);
+    })
+    .catch(function (err) {
+      log.error(err);
+      res.send(500);
     });
 });
 
